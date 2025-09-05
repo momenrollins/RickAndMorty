793d25465eab0c377bf25ce090c1b3c657146c63
Mo'men <momenrefaat22@gmail.com>
2025-09-05 21:27:01 +0300

    feat: Add error state and loading indicator to CharacterListScreen

--- app/src/main/java/com/momen/rickandmorty/presentation/ui/characterlist/CharacterListScreen.kt
+++ app/src/main/java/com/momen/rickandmorty/presentation/ui/characterlist/CharacterListScreen.kt
@@ -2,16 +2,24 @@ package com.momen.rickandmorty.presentation.ui.characterlist
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -65,8 +73,6 @@ fun CharacterListScreen(
             state = listState,
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-
-
             items(state.characters) { character ->
                 CharacterListItem(
                     character = character,
@@ -78,6 +84,29 @@ fun CharacterListScreen(
                     LoadingItem()
                 }
             }
+        }
+        if (state.error.isNotBlank()) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = state.error,
+                    color = MaterialTheme.colorScheme.error,
+                    textAlign = TextAlign.Center,
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(onClick = { viewModel.onEvent(CharacterListEvent.Refresh) }) {
+                    Text("Retry")
+                }
+            }
+        }
+
+        if (state.isLoading) {
+            CircularProgressIndicator(
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
         }
     }
 }
